6c5314e575f6725806b68256a22c3828e8b8e89e
Jimmy8378 <135547239+Jimmy8378@users.noreply.github.com>
2023-11-07 20:08:03 +0100

    Update HelloWorld.php

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,9 +1,13 @@
 <?php
 echo "<h1>Hello World <br></h1>";
-echo" <input type='integer'name='table' alt='table'>";
+echo " <form method='post' alt='formulaire table' alt='formulaire table'> ";
+echo " <input type='integer'  name='table' alt='table'> <br>";
+echo " <button name='envoyer' onclick='submit'></form>";
+echo "<br><center>";
 if(isset($_POST['table'])){
 echo " Table de :".$_POST['table'];
-for($i=0;10;$i++){
+for($i=0;$i<=10;$i++){
 echo "<br> ".$_POST['table']. " fois ".$i." égale ".$_POST['table']*$i;
 };
+echo "</center>";
 };
